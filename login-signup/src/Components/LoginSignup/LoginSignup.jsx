be1b3a883693cc5618fefbd84fa20c69aced3fb9
Ramilbey <ramilbey505711503@gmail.com>
2025-05-25 21:01:06 +0800

    create component

--- login-signup/src/Components/LoginSignup/LoginSignup.jsx
+++ login-signup/src/Components/LoginSignup/LoginSignup.jsx
@@ -1,1 +1,11 @@
-rafc
+import React from 'react';
+
+const LoginSignup = () => {
+    return (
+        <div>
+            {/* Your code here */}
+        </div>
+    );
+}
+
+export default LoginSignup;
