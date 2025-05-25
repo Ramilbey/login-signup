d75e588db2f3e58b8c3f65fce1a0663f6ca87319
Ramilbey <ramilbey505711503@gmail.com>
2025-05-25 21:19:21 +0800

    generate the htmml

--- login-signup/src/Components/LoginSignup/LoginSignup.jsx
+++ login-signup/src/Components/LoginSignup/LoginSignup.jsx
@@ -1,13 +1,34 @@
-import './LoginSignup.css'
-import React from 'react';
-
+import "./LoginSignup.css";
+import email from "../Assests/email.png";
+import password from "../Assests/password.png";
+import person from "../Assests/person.png";
+import React from "react";
 
 const LoginSignup = () => {
-    return (
-        <div>
-            {/* Your code here */}
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="text">Sign Up</div>
+        <div className="underline">underline</div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+          <img src="" alt="" />
+          <input type="text" />
         </div>
-    );
-}
+
+        <div className="input">
+          <img src="" alt="" />
+          <input type="email" />
+        </div>
+
+        <div className="input">
+          <img src="" alt="" />
+          <input type="password" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default LoginSignup;
